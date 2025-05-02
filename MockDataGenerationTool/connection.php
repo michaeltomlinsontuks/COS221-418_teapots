ee7c48341cfd506618700f0b67c394039eb7314d
michaeltomlinsontuks <159887992+michaeltomlinsontuks@users.noreply.github.com>
2025-05-02 20:03:56 +0200

    Built in config file functionality for the mock data generator

---
+++ MockDataGenerationTool/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once 'config.php';
+
+function getConnection() {
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
+?>
