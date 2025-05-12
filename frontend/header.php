55f3143883755034e83dcc5b50e7257bd31a332e
Wilmar-Smit <u24584216@tuks.co.za>
2025-05-12 14:15:42 +0200

    page builder should now work as expected Wsmit

--- frontend/header.php
+++ frontend/header.php
@@ -4,6 +4,6 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="W.css">
-    <link rel="stylesheet" href="D.css">
+    <link rel="stylesheet" href="cssFiles/W.css">
+    <link rel="stylesheet" href="cssFiles/D.css">
 </head>
--- frontend/pagebuilder.php
+++ frontend/pagebuilder.php
@@ -4,34 +4,44 @@ require_once("header.php")
 
 <body>
 
-    <?php
-    // the get is all that is changed on links 
-    //link pagebuilder.php?page="####"
-    //onclick="location.href = 'pagebuilder.php?page=products'" />
-    //can do this in the items themselves
-    if (isset($_GET['page']))
-        switch ($_GET['page']) {
-            case ("login"):
-                include_once("pages/login.php");
-                break;
-            case ("signUp"):
-                include_once("pages/signup.php");
-                break;
-            case ("products"):
-                include_once("pages/products.php");
-                break;
-            case ("view"):
-                include_once("pages/view.php");
-                break;
-            default:
-                include_once("pages/products.php");
-                break;
-        }
-    else
-        include_once "pages/products.php";
-    ?>
-    <?php
-    require_once("footer.php")
-        ?>
+    <div class="headingBar">
+        <?php
+        if (isset($_GET['page']))
+            echo $_GET['page'];
+        else
+            echo "products"
+                ?>
+        </div>
 
+        <div class="contentContainer">
+            <?php
+        // the get is all that is changed on links 
+        //link pagebuilder.php?page="####"
+        //onclick="location.href = 'pagebuilder.php?page=products'" />
+        //can do this in the items themselves
+        if (isset($_GET['page']))
+            switch ($_GET['page']) {
+                case ("login"):
+                    include_once("pages/login.php");
+                    break;
+                case ("signUp"):
+                    include_once("pages/signup.php");
+                    break;
+                case ("products"):
+                    include_once("pages/products.php");
+                    break;
+                case ("view"):
+                    include_once("pages/view.php");
+                    break;
+                default:
+                    include_once("pages/products.php");
+                    break;
+            }
+        else
+            include_once "pages/products.php";
+        ?>
+        <?php
+        require_once("footer.php")
+            ?>
+    </div>
 </body>
