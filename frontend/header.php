2968d149ad026dbca3e6e7ffa0b5e0860ef7e6d1
Wilmar-Smit <u24584216@tuks.co.za>
2025-05-12 13:22:42 +0200

    added the css templates and the header file

--- frontend/header.php
+++ frontend/header.php
@@ -4,5 +4,6 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+    <link rel="stylesheet" href="W.css">
+    <link rel="stylesheet" href="D.css">
 </head>
