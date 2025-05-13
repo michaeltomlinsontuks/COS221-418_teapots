4561329b956a25c1b40209f826909bb4c0a05be3
michaeltomlinsontuks <159887992+michaeltomlinsontuks@users.noreply.github.com>
2025-05-13 09:05:02 +0200

    Removed API Config - old credentials - no worries

--- API/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//This should never get onto the github, a commit fail will mean we need to rotate our authentication
-
-//config.php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', '418_teapots_MOCKDATA');
-?>
