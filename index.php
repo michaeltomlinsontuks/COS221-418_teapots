ae26bbf186e0f4d31d1fa168e131c407ce9da4b4
Wilmar-Smit <u24584216@tuks.co.za>
2025-05-25 16:16:54 +0200

    about us changes

--- index.php
+++ index.php
@@ -1,19 +1,37 @@
 <?php
-header("Location: http://localhost/teapots/frontend/pagebuilder.php?page=login");
+//header("Location: http://localhost/teapots/frontend/pagebuilder.php?page=login");
 // will move to what is wheateley configure on your own 
-exit(); // Always exit after a redirect
+//exit(); // Always exit after a redirect
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic HTML Page</title>
+    <title>Teapots pricecheck</title>
+    <link rel="stylesheet" href="index.css">
+
 </head>
+
 <body>
-    <h1>Welcome to My Page</h1>
-    <p>This is a simple HTML page structure.</p>
+    <div class="container">
+        <img src="images/teapotsLogoPot.png" alt="">
+    </div>
+    <div class="aboutUsDiv">
+        <input type="button" onclick="goToProducts()" value="Products">
+        <input type="button" onclick="goToAboutUs()" value="about us">
+    </div>
 </body>
+<script>
+    function goToProducts() {
+        window.location.href = "http://localhost/teapots/frontend/pagebuilder.php?page=login";
+    }
+    function goToAboutUs() {
+        window.location.href = "http://localhost/teapots/aboutUs.php";
+    }
+</script>
+
 </html>
