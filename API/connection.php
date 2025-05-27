80a098c40a5662a025b003ba3ddd62c9e0786759
Damian <damianmoustakis@gmail.com>
2025-05-27 07:50:41 +0200

    Again patching the local out

--- API/connection.php
+++ API/connection.php
@@ -1,6 +1,5 @@
 <?php
-// Change this line
-include_once(__DIR__ . '/../config.php');
+include_once 'config.php';
 
 function getConnection() {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
